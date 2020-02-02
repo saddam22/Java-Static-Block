10f150daa7b5612c29d7c39bd49e9a6705cafab0
Md.Saddam Hossain <csesolve6548@gmail.com>
2020-02-02 16:53:25 +0600

    Static Block

---
+++ src/StaticBlock/StaticBlock.java
@@ -0,0 +1,12 @@
+
+package StaticBlock;
+
+public class StaticBlock {
+    
+    static{
+        System.out.println("Static Blocked is Invoked!!");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello Wanbahjahkas!!!");
+    }
+}
